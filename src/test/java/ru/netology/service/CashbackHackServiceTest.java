7533850a8ea1908d93d0d5b34cc6cc5dede22ddc
SergGrebnev <serggrebnev79@gmail.com>
2024-08-05 12:23:04 +0700

    Create test junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,9 +1,22 @@
 package ru.netology.service;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemainMoreThanBoundary() {
+        CashbackHackService testCashback = new CashbackHackService();
+
+        int actual = testCashback.remain(1001);
+        int expected = 0;
+
+        Assert.assertEquals(expected, actual);
+
+    }
 
     @Test
     public void testRemainLessThanBoundary() {
@@ -12,7 +25,7 @@ public class CashbackHackServiceTest {
         int actual = testCashback.remain(999);
         int expected = 1;
 
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
 
     }
 
@@ -23,18 +36,7 @@ public class CashbackHackServiceTest {
         int actual = testCashback.remain(1000);
         int expected = 0;
 
-        Assert.assertEquals(actual, expected);
-
-    }
-
-    @Test
-    public void testRemainMoreThanBoundary() {
-        CashbackHackService testCashback = new CashbackHackService();
-
-        int actual = testCashback.remain(1001);
-        int expected = 0;
-
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
 
     }
 }
